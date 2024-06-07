7a2fecd0af8046047a3673dc3829ab11dc3dbe3e
Gabriel Birzu <gbirzu@gmail.com>
2024-06-07 15:42:17 -0700

    Added reference genome mapping script.

---
+++ scripts/pg_map_to_reference.py
@@ -0,0 +1,158 @@
+import argparse
+import subprocess
+import glob
+import pickle
+import os
+import utils
+import numpy as np
+import pandas as pd
+import seq_processing_utils as seq_utils
+import alignment_tools as align_utils
+import pangenome_utils as pg_utils
+import time
+from pangenome_utils import PangenomeMap
+from Bio import SeqIO
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+
+def get_consensus_seq_records(og_table, aln_dir, ext='aln'):
+    sag_columns = [col for col in og_table.columns if 'Uncmic' in col]
+    consensus_seq_records = []
+    for scog_id in og_table.index:
+        # Get parent OG ID
+        if '-' in scog_id:
+            og_id, subcluster_id = scog_id.split('-')
+        else:
+            og_id = scog_id
+        f_aln = f'{aln_dir}{og_id}_{ext}.fna'
+        if os.path.exists(f_aln):
+            print(scog_id, og_id)
+            gene_ids = pg_utils.read_gene_ids(og_table.loc[scog_id, sag_columns], drop_none=True)
+            aln = AlignIO.read(f_aln, 'fasta')
+            scog_aln = align_utils.get_subsample_alignment(aln, gene_ids)
+            consensus_seq_arr = seq_utils.get_consensus_seq(scog_aln, seq_type='codons', keep_gaps=False)
+            seq_record = SeqRecord(Seq(''.join(consensus_seq_arr)), id=f'{scog_id}', description='consensus seq')
+            consensus_seq_records.append(seq_record)
+    return consensus_seq_records
+
+def find_orthologous_groups_rbh(f_blast_results):
+    blast_results = seq_utils.read_blast_results(f_blast_results, extra_columns=['qseqlen', 'sseqlen'])
+    rbh_df = pg_utils.filter_reciprocal_best_hits(blast_results)
+    return rbh_df
+
+def make_rbh_dict(ref_rbh_files):
+    rbh_dict = {}
+    for f_results in ref_rbh_files:
+        ref_id = f_results.split('/')[-1].strip('_merged_blast_results.tsv')
+        ref_rbh_df = find_orthologous_groups_rbh(f_results)
+        ref_rbh_dict = {}
+        for i, row in ref_rbh_df.iterrows():
+            ref_rbh_dict[row['qseqid']] = row['sseqid']
+            ref_rbh_dict[row['sseqid']] = row['qseqid']
+        rbh_dict[ref_id] = ref_rbh_dict
+    return rbh_dict
+
+
+def update_og_table(og_table, rbh_dict, ref_annotations):
+    old_columns = list(og_table.columns)
+    columns = ['gene', 'CYA_tag', 'CYB_tag'] + list(og_table.columns)
+
+    for og_id in og_table.index:
+        ref_locus_tag, ref_id = get_ref_locus_tag(og_id, rbh_dict, 'CP000239') 
+        if ref_locus_tag is not None:
+            og_table.loc[og_id, 'CYA_tag'] = ref_locus_tag
+            gene = get_locus_tag_gene(ref_locus_tag, ref_annotations[ref_id])
+            og_table.loc[og_id, 'gene'] = gene
+
+        ref_locus_tag, ref_id = get_ref_locus_tag(og_id, rbh_dict, 'CP000240') 
+        if ref_locus_tag is not None:
+            og_table.loc[og_id, 'CYB_tag'] = ref_locus_tag
+            gene = get_locus_tag_gene(ref_locus_tag, ref_annotations[ref_id])
+            if gene is not None:
+                og_table.loc[og_id, 'gene'] = gene # Prefer OS-B' gene name
+
+    return og_table.reindex(columns=columns)
+
+def get_ref_locus_tag(og_id, rbh_dict, default_ref):
+    if og_id in rbh_dict[default_ref]:
+        locus_tag = rbh_dict[default_ref][og_id]
+        ref_id = default_ref
+    else:
+        locus_tag = None
+        ref_id = None
+        for ref_id in rbh_dict:
+            if og_id in rbh_dict[ref_id]:
+                locus_tag = rbh_dict[ref_id][og_id]
+                break
+    return locus_tag, ref_id
+
+def get_locus_tag_gene(locus_tag, ref_annotation):
+    locus_annot = ref_annotation[locus_tag]
+    if 'gene' in locus_annot.qualifiers:
+        gene = locus_annot.qualifiers['gene'][0]
+    else:
+        gene = None
+    return gene
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-D', '--data_dir', help='Directory with output of previous clustering steps.')
+    parser.add_argument('-O', '--output_dir', help='Directory in which to store intermediate results.')
+    parser.add_argument('-b', '--best_hits_file', default=None, help='File with paths to BLAST results files.')
+    parser.add_argument('-e', '--extension', default='trimmed_aln', help='Alignment file extension.')
+    parser.add_argument('-g', '--orthogroup_table', help='File with orthogroup table.')
+    parser.add_argument('-o', '--output_file', default=None, help='Output file.')
+    parser.add_argument('-p', '--prefix', default='sscs', help='Files prefix.')
+    parser.add_argument('-r', '--references_file', default=None, help='File with paths to Genbank files for each reference.')
+    parser.add_argument('-u', '--updates_file', default=None, help='File where mapping of OG ids is stored.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print mapping.')
+    parser.add_argument('--genbank_extension', default='genbank', help='Extension for genbank files.')
+    parser.add_argument('--random_seed', default=12345, type=int, help='Random seed for reproducibility.')
+    parser.add_argument('--export_ref_seqs', action='store_true', help='Export CDS seqs from list of reference genome files in Genbank format.')
+    parser.add_argument('--export_og_consensus_seqs', action='store_true', help='Export consensus seq for OGs in OG table.')
+    parser.add_argument('--map_rbh', action='store_true', help='Map genes to reference using RBH.')
+    args = parser.parse_args()
+
+
+    if args.export_ref_seqs == True:
+        with open(args.references_file, 'r') as in_handle:
+            for line in in_handle.readlines():
+                f_ref = line.strip()
+                ref_id = f_ref.split('/')[-1].strip(f'.{args.genbank_extension}')
+                ref_cds_seqs = utils.extract_genbank_cds_seqs(f_ref, add_descriptions=False)
+                seq_utils.write_seqs_dict(ref_cds_seqs, f'{args.output_dir}{ref_id}_cds.fna')
+
+    elif args.export_og_consensus_seqs == True:
+        np.random.seed(args.random_seed)
+        pangenome_map = PangenomeMap(f_orthogroup_table=args.orthogroup_table)
+        og_table = pangenome_map.og_table
+
+        consensus_seq_records = get_consensus_seq_records(og_table, f'{args.data_dir}trimmed_aln/', ext=args.extension)
+        SeqIO.write(consensus_seq_records, args.output_file, 'fasta')
+
+    elif args.map_rbh == True:
+        # Make RBH dict
+        ref_rbh_files = []
+        with open(args.best_hits_file, 'r') as in_handle:
+            for line in in_handle.readlines():
+                f_results = line.strip()
+                ref_rbh_files.append(f_results)
+        rbh_dict = make_rbh_dict(ref_rbh_files)
+
+        # Read ref annotations
+        ref_annotations = {}
+        with open(args.references_file, 'r') as in_handle:
+            for line in in_handle.readlines():
+                f_ref = line.strip()
+                ref_id = f_ref.split('/')[-1].strip(f'.{args.genbank_extension}')
+                ref_cds = utils.read_genbank_cds(f_ref)
+                ref_annotations[ref_id] = ref_cds
+
+        # Update OG table
+        pangenome_map = PangenomeMap(f_orthogroup_table=args.orthogroup_table)
+        og_table = pangenome_map.og_table
+        updated_og_table = update_og_table(og_table, rbh_dict, ref_annotations)
+        updated_og_table.to_csv(args.output_file, sep='\t')
---
+++ scripts/sbatch_map_ogs_to_references.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -l
+#SBATCH --job-name=map_ogs_to_references
+#SBATCH --mail-type=END         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=gbirzu@stanford.edu # Where to send mail
+#SBATCH --time=24:00:00 # Time limit hrs:min:sec (max 24 hrs on Sherlock normal queue)
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16GB
+#SBATCH --partition=hns
+#SBATCH --output=slurm_map_ogs_to_references_%A.out
+
+ml ncbi-blast+
+
+#data_dir='../results/single-cell/sscs_pangenome/'
+data_dir='../results/single-cell/sscs_pangenome_v2/'
+blast_dir="../results/tests/pangenome_construction/v2/_blastn_results/"
+f_updated_og_table='../results/tests/pangenome_construction/v2/test_mapped_orthogroup_table.tsv'
+mkdir -p ${blast_dir}
+num_threads=4
+
+# Make ref files list
+refs_dir='../data/reference_genomes/'
+refs_files=("${refs_dir}CP000239.genbank" "${refs_dir}CP000240.genbank")
+f_ref_files="${blast_dir}synecho_ref_files.txt"
+if [ -f "${f_ref_files}" ]
+then
+    rm -f ${f_ref_files}
+fi
+for f_ref in ${refs_files[*]}
+do
+    echo "${f_ref}" >> ${f_ref_files}
+done
+
+# Export seqs
+python3 pg_map_to_reference.py -O ${blast_dir} -r ${f_ref_files} --export_ref_seqs
+#f_og_table="${data_dir}filtered_orthogroups/sscs_filtered_orthogroup_presence.tsv"
+f_og_table="../results/tests/pangenome_construction/v2/test_orthogroup_table.tsv"
+python3 pg_map_to_reference.py -D ${data_dir} -g ${f_og_table} -o "${blast_dir}sscs_single_copy_consensus_seqs.fna" --export_og_consensus_seqs
+
+# Find BLAST RBH against references
+echo "Making SSCS consensus seqs database..."
+makeblastdb -dbtype nucl -out "${blast_dir}sscs_single_copy_consensus_seqs.blastdb" -in "${blast_dir}sscs_single_copy_consensus_seqs.fna"
+
+f_rbh="${blast_dir}blast_result_files.txt"
+if [ -f ${f_rbh} ]
+then
+    rm -f ${f_rbh}
+fi
+
+for f_ref in ${refs_files[*]}
+do
+    # Make reference database
+    ref_id=$(echo ${f_ref} | sed 's/.*\///g' | sed 's/\.genbank//g')
+    f_blastdb="${blast_dir}${ref_id}_cds.blastdb"
+    echo "Making ${ref_id} BLAST database..."
+    makeblastdb -dbtype nucl -out ${f_blastdb} -in "${blast_dir}${ref_id}_cds.fna"
+
+    # Run BLAST
+    echo "Forward hit search..."
+    f_forward="${blast_dir}sscs_single_copy-${ref_id}_blast_results.tsv"
+    blastn -task blastn -num_threads ${num_threads} -word_size 6 -evalue 1E-3 \
+            -outfmt '6 std qlen slen' -qcov_hsp_perc 75 \
+            -db ${f_blastdb} \
+            -out ${f_forward} \
+            -query "${blast_dir}sscs_single_copy_consensus_seqs.fna"
+
+    echo "Reverse hit search..."
+    f_reverse="${blast_dir}${ref_id}-sscs_single_copy_blast_results.tsv"
+    blastn -task blastn -num_threads ${num_threads} -word_size 6 -evalue 1E-3 \
+            -outfmt '6 std qlen slen' -qcov_hsp_perc 75 \
+            -db "${blast_dir}sscs_single_copy_consensus_seqs.blastdb" \
+            -out ${f_reverse} \
+            -query "${blast_dir}${ref_id}_cds.fna"
+
+    f_merged="${blast_dir}${ref_id}_merged_blast_results.tsv"
+    cat ${f_forward} ${f_reverse} > ${f_merged}
+    echo ${f_merged} >> ${f_rbh}
+
+    # Clean up
+    rm -f ${f_forward}
+    rm -f ${f_reverse}
+done
+
+# Map OG IDs to reference
+python3 pg_map_to_reference.py -b ${f_rbh} -g ${f_og_table} -r ${f_ref_files} -o ${f_updated_og_table} --map_rbh
+
+
+# Clean up
+#cp ${data_dir}fine_scale_og_clusters/*.fna ${data_dir}filtered_orthogroups/
+#cp ${f_og_table} ${data_dir}filtered_orthogroups/ 
+rm -f ${f_ref_files}
+rm -f ${f_rbh}
+
